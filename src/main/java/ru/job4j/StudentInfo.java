141ef83d225108f8aff2ed1d63a993cd27c18cb5
dmax <dmax45@gmail.com>
2022-07-06 11:59:08 +0500

    Фиксация изменений на github [#243564]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Danila Novikov");
+        System.out.println("29.04.1995");
+    }
+}
