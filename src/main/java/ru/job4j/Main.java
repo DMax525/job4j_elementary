8410ac17886ce496727ee23fb048ef58b30fe3c6
dmax <dmax45@gmail.com>
2022-07-09 19:59:21 +0500

    Расстояние между точками в системе координат [#188 #320029]

--- src/main/java/ru/job4j/Main.java
+++ src/main/java/ru/job4j/Main.java
@@ -2,6 +2,9 @@ package ru.job4j;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        float f = 12358.7f;
+        int i = 45981;
+        short rsl = (short) (f + i);
+        System.out.println(rsl);
     }
 }
