533d4b8963612b3f0c3147c1254b92803749b053
dmax <dmax45@gmail.com>
2022-07-14 11:06:18 +0500

    4.3.2. Сокращенный оператор || [#333784 #321722]

--- src/main/java/ru/job4j/condition/Club.java
+++ src/main/java/ru/job4j/condition/Club.java
@@ -2,8 +2,7 @@ package ru.job4j.condition;
 
 public class Club {
     public static void permission(boolean hasMoney, boolean beFriend) {
-        boolean oneof = hasMoney | beFriend;
-        if (oneof) {
+        if (hasMoney || beFriend) {
             System.out.println("I can go to the club.");
         } else {
             System.out.println("I can't.");
