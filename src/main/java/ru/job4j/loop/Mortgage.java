47694df5093c9580e3ed40566680b1544d8ab73c
dmax <dmax45@gmail.com>
2022-07-18 11:15:49 +0500

    5.7. Ипотека [#156315]

---
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -0,0 +1,15 @@
+package ru.job4j.loop;
+
+public class Mortgage {
+
+    public static int year(double amount, int salary, double percent) {
+        int year = 0;
+        amount += amount * (percent * 0.01);
+        while (amount >= 0) {
+            year++;
+            amount -= salary;
+            amount += amount * (percent * 0.01);
+        }
+        return year;
+    }
+}
